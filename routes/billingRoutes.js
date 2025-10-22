b5d6ee05531926f0b50a2c4ab7aed97f5d34a8fc
XenderMd <denis.stavilamd@gmail.com>
2025-10-22 21:21:25 +0300

    adding credits to the user

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,13 +3,26 @@ const stripe = require('stripe')(key.stripeSecretKey);
 
 module.exports = (app) => {
   app.post('/api/stripe', async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: 'usd',
-      description: '5$ for 5 credits',
-      source: req.body.id,
-    });
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must log in!' });
+    }
+    try {
+      const charge = await stripe.charges.create({
+        amount: 500,
+        currency: 'usd',
+        description: '5$ for 5 credits',
+        source: req.body.id,
+      });
 
-    console.log(charge);
+      req.user.credits += 5;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (error) {
+      console.log('error billing user', error);
+      res.status(500).send({
+        error: 'An internal error has occured, please try again later',
+      });
+    }
   });
 };
