eeea68da9c1811eeaa3c182fba6e38511eb618fd
XenderMd <denis.stavilamd@gmail.com>
2025-10-22 18:43:20 +0300

    creating charges & bodyparser middleware

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/Users');
 require('./services/passport');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,3 +1,6 @@
+const key = require('../config/keys');
+const stripe = require('stripe')(key.stripeSecretKey);
+
 module.exports = (app) => {
   app.post('/api/stripe', (req, res) => {});
 };
